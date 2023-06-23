b491258e051677db7e85d227575fa8b41b372aeb
prashantsaran <72952601+prashantsaran@users.noreply.github.com>
2023-06-23 19:06:53 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './pages/home/main/main.component';
+import { CartComponent } from './pages/home/cart/cart.component';
+import { IteminfoComponent } from './pages/home/iteminfo/iteminfo.component';
+import { SearchPageComponent } from './pages/home/search-page/search-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: LoginComponent},
+  {path:'home',component:MainComponent},
+  {path:'cart',component:CartComponent},
+  {path:'iteminfo',component:IteminfoComponent},
+  {path: 'search-page', component: SearchPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
